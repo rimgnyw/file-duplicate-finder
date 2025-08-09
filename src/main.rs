ad7d3ec1fb7658388d2bb96dc8a38390928a77a3
rimgnyw <markus.wingmyr@telia.com>
2025-08-09 16:29:41 +0200

    add windows specific path formatting

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,13 @@ fn main() {
         return;
     }
 
-    let start = Instant::now();
+    // let start = Instant::now();
 
     match run_scan(&base_dir.unwrap()) {
         Ok(result) => {
             let r = write_log_to_file(&result, "./scan.log");
             if r.is_err() {
                 eprintln!("Failed to write log");
-            } else {
-                println!(
-                    "log written to {:?}",
-                    PathBuf::from("./scan.log").canonicalize().unwrap()
-                );
             }
         }
 
@@ -45,25 +40,57 @@ fn main() {
         }
     }
 
-    let duration = start.elapsed();
-    println!("Time Elapse: {:?}", duration);
+    // let duration = start.elapsed();
+    // println!("Time Elapse: {:?}", duration);
 }
 
-fn write_log_to_file(
-    map: &HashMap<OsString, Vec<PathBuf>>,
-    file_path: &str,
-) -> std::io::Result<()> {
-    let file = File::create(file_path)?;
+#[cfg(not(target_os = "windows"))]
+fn write_log_to_file(map: &HashMap<OsString, Vec<PathBuf>>, log_path: &str) -> std::io::Result<()> {
+    let file = File::create(log_path)?;
     let mut writer = BufWriter::new(file);
 
     for (filename, paths) in map {
         if paths.len() > 1 {
             writeln!(writer, "{}:", filename.to_string_lossy())?;
             for path in paths {
-                writeln!(writer, "  {}", path.display())?;
+                writeln!(writer, "  {}", path.to_owned().display())?;
             }
         }
     }
+    println!(
+        "Log written to:\n{}",
+        PathBuf::from(log_path).canonicalize().unwrap().display()
+    );
+
+    Ok(())
+}
+
+#[cfg(target_os = "windows")]
+fn normalise_paths(path: PathBuf) -> PathBuf {
+    let s = path.to_string_lossy();
+    if s.starts_with(r"\\?\") {
+        return PathBuf::from(&s[4..]);
+    }
+    path
+}
+
+#[cfg(target_os = "windows")]
+fn write_log_to_file(map: &HashMap<OsString, Vec<PathBuf>>, log_path: &str) -> std::io::Result<()> {
+    let file = File::create(log_path)?;
+    let mut writer = BufWriter::new(file);
+
+    for (filename, paths) in map {
+        if paths.len() > 1 {
+            writeln!(writer, "{}:", filename.to_string_lossy())?;
+            for path in paths {
+                writeln!(writer, "  {}", normalise_paths(path.to_owned()).display())?;
+            }
+        }
+    }
+    println!(
+        "Log written to:\n{}",
+        normalise_paths(PathBuf::from(log_path).canonicalize().unwrap()).display()
+    );
 
     Ok(())
 }
